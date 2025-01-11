a85a367f6c36447364e1bd4d15a44e03d5f94f67
HarryShengCS <harryshengcoding@gmail.com>
2025-01-12 10:00:23 +1300

    feat (firebase): implemented firebase sdk with authentication.

---
+++ liftlog-web-client/app/firebase/firebase.ts
@@ -0,0 +1,51 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  User,
+} from "firebase/auth"
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCDfPNJbD4QruCyiqKpMkcBKj--ExxX9cU",
+  authDomain: "liftlog-97567.firebaseapp.com",
+  projectId: "liftlog-97567",
+  appId: "1:222314261215:web:709546fee8b59363fc176a",
+  measurementId: "G-EX5LGHKWV2",
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
+const auth = getAuth(app)
+
+/**
+ * Signs the user in with a Google popup.
+ * @returns A promise that resolves with the user's credentials.
+ */
+export function signInWithGoogle() {
+  return signInWithPopup(auth, new GoogleAuthProvider())
+}
+
+/**
+ * Signs the user out.
+ * @returns A promise that resolves when the user is signed out.
+ */
+export function signOut() {
+  return auth.signOut()
+}
+
+/**
+ * Trigger a callback when user auth state changes.
+ * @returns A function to unsubscribe callback.
+ */
+export function onAuthStateChangedHelper(
+  callback: (user: User | null) => void
+) {
+  return onAuthStateChanged(auth, callback)
+}
