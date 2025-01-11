91a0e741acedc2efe1540a5ac429ffe6f156202d
HarryShengCS <harryshengcoding@gmail.com>
2025-01-12 10:53:01 +1300

    feat (sign in): added sign in with google.

--- liftlog-web-client/app/navbar/navbar.tsx
+++ liftlog-web-client/app/navbar/navbar.tsx
@@ -1,19 +1,40 @@
-import Image from "next/image"
+"use client"
+
+import SignIn from "./signIn"
 import Link from "next/link"
 
 import styles from "./navbar.module.css"
+//import Upload from "./upload"
+import { useEffect, useState } from "react"
+import { onAuthStateChangedHelper } from "../firebase/firebase"
+import { User } from "firebase/auth"
 
-export default function Navbar() {
+function NavBar() {
+  // Initialize user state
+  const [user, setUser] = useState<User | null>(null)
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedHelper((user) => {
+      setUser(user)
+    })
+
+    return () => unsubscribe()
+  }, [])
+
   return (
     <nav className={styles.nav}>
       <Link href="/">
-        <Image
-          width={90}
-          height={90}
-          src="/liftlog-logo.svg"
-          alt="LiftLog Logo"
-        />
+        <span className={styles.logoContainer}>
+          <img
+            className={styles.logo}
+            src="/liftlog-logo.svg"
+            alt="LiftLog Logo"
+          />
+        </span>
       </Link>
+      <SignIn user={user} />
     </nav>
   )
 }
+
+export default NavBar
